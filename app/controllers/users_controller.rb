f47095a73000407c748998e9d53f4dd9c4349155
throughtronik <to.isla@gmail.com>
2021-04-08 00:51:30 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,38 @@
-# Управление юзерами
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://www.bikkinpanda.com/wp-content/uploads/2018/04/gravatar-1-810x450.png'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Hello World!"
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://www.bikkinpanda.com/wp-content/uploads/2018/04/gravatar-1-810x450.png'
+    )
+
+     @questions = [
+      Question.new(text: 'How r u?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'What is the sense of life?', created_at: Date.parse('27.03.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'user_avatar.jpg'
+    end
+  end
 end
