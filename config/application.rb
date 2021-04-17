b91ef320c108554daddc5eece48518b2f3e348ba
throughtronik <to.isla@gmail.com>
2021-04-17 16:08:37 +0300

    removed sprockets reqs

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -34,5 +34,6 @@ module Askme
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # config.assets.enabled = false
   end
 end
