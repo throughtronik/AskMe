207fa57beb87f6cd27e84946b4e1ec0448c38fbf
throughtronik <to.isla@gmail.com>
2021-04-22 01:32:58 +0300

    question author relation implemented

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
 
+  belongs_to :author, class_name: :User, optional: true
   validates :text, presence: true, length: { maximum: 255, message: "is more than 255 symbols" }
 end
