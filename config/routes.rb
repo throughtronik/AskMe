81e223caf3e113f5839b95623b7fc5269a7983dd
throughtronik <to.isla@gmail.com>
2021-06-06 01:31:51 +0300

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resources :session, only: [:create, :destroy, :new]
+  resource :session, only: [:create, :destroy, :new]
   resources :questions, except: [:show, :new, :index]
   resources :hashtags, only: [:show]
 
